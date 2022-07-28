6dcb83d85acb05278c34e6f39ac72ba0d5c3d1cd
DESKTOP-EDQPJ85\baro <kijewoo143@gmail.com>
2022-07-28 18:51:10 +0900

    lvl1 - 64061 크레인 인형뽑기 게임

--- programmers_level_1/programmers_level_1/programmers_level_1.cpp
+++ programmers_level_1/programmers_level_1/programmers_level_1.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <stack>
 
 using namespace std;
+
+int solution12931(int n);
+int solution64061(vector<vector<int>> board, vector<int> moves);
 
 int main()
 {
     {
+        vector<vector<int>> board = { {0, 0, 0, 0, 0},{0, 0, 1, 0, 3},{0, 2, 5, 0, 1},{4, 2, 4, 4, 2},{3, 5, 1, 3, 1} };
+        vector<int> moves = { 1,5,3,5,1,2,1,4 };
 
+        int result = solution64061(board, moves);
     }
 }
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/12931
+// 자릿수 더하기
 int solution12931(int n)
 {
-    // 자릿수 더하기
     int temp = n;
     int sum = 0;
 
@@ -26,3 +34,38 @@ int solution12931(int n)
     return sum;
 }
 
+// https://school.programmers.co.kr/learn/courses/30/lessons/64061
+// 크레인 인형뽑기 게임
+int solution64061(vector<vector<int>> board, vector<int> moves)
+{
+	int answer = 0;
+	vector<int> stack;
+	stack.reserve(moves.size());
+	vector<int> rowIndex(board.size());
+
+	for (int i = 0; i < moves.size(); ++i)
+	{
+		int col = moves[i] - 1;
+
+		for (int row = rowIndex[col]; row < board.size(); rowIndex[col] = ++row)
+		{
+			if ((board[row][col] != 0))
+			{
+				if (stack.empty() || board[row][col] != stack.back())
+				{
+					stack.push_back(board[row][col]);
+				}
+				else
+				{
+					stack.pop_back();
+					answer += 2;
+				}
+
+				board[row][col] = 0;
+				break;
+			}
+		}
+	}
+
+	return answer;
+}
